93fb8d6f198b2c69e8fa1baec8c971725db8c43d
fernanda <fe.pobletel@duocuc.cl>
2024-06-27 22:00:04 -0400

    cambiado a lista de pokemones(falta decorar))

--- assets/js/pokescript.js
+++ assets/js/pokescript.js
@@ -1,27 +1,50 @@
 $(document).ready(function() {
-    const apiURL = 'https://pokeapi.co/api/v2/version/';
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+    const limit = 100; // Obtener 100 Pokémon
 
-    function fetchPokemonGames() {
+    function fetchPokemon() {
         $.ajax({
-            url: apiURL,
+            url: `${apiUrl}?limit=${limit}`,
             method: 'GET',
             success: function(response) {
-                const games = response.results;
-                const gamesList = $('#pokemon-games-list');
+                const pokemonList = response.results;
+                const pokemonContainer = $('#pokemon-list');
 
-                gamesList.empty();
+                pokemonContainer.empty();
 
+                pokemonList.forEach(function(pokemon) {
+                    $.ajax({
+                        url: pokemon.url,
+                        method: 'GET',
+                        success: function(pokemonData) {
+                            const name = pokemonData.name;
+                            const imageUrl = pokemonData.sprites.front_default;
+                            const types = pokemonData.types.map(type => type.type.name).join(', ');
 
-                games.forEach(function(game) {
-                    const listItem = $('<li></li>').text(game.name);
-                    gamesList.append(listItem);
+                            const card = `
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <img src="${imageUrl}" class="card-img-top" alt="${name}">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${name}</h5>
+                                            <p class="card-text">Tipo(s): ${types}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            `;
+                            pokemonContainer.append(card);
+                        },
+                        error: function() {
+                            console.error(`Error al obtener los datos de ${pokemon.name}`);
+                        }
+                    });
                 });
             },
             error: function() {
-                alert('Error al obtener los juegos de Pokémon.');
+                console.error('Error al obtener la lista de Pokémon');
             }
         });
     }
 
-    fetchPokemonGames();
+    fetchPokemon();
 });
